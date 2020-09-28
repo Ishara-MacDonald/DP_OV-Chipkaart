25a009bc69b62ea00cfcd923633b910f4ffd3dbf
Ishara-Mac <isharamacdonald@hotmail.com>
2020-09-28 19:44:17 +0200

    Finished S5

--- S-Line/S5 - Ishara Uitwerkingen.sql
+++ S-Line/S5 - Ishara Uitwerkingen.sql
@@ -6,9 +6,9 @@
 -- Tijmen Muller (tijmen.muller@hu.nl)
 -- André Donk (andre.donk@hu.nl)
 --
-    --
-        -- Opdracht: schrijf SQL-queries om onderstaande resultaten op te vragen,
-    -- aan te maken, verwijderen of aan te passen in de database van de
+--
+-- Opdracht: schrijf SQL-queries om onderstaande resultaten op te vragen,
+-- aan te maken, verwijderen of aan te passen in de database van de
 -- bedrijfscasus.
 --
     -- Codeer je uitwerking onder de regel 'DROP VIEW ...' (bij een SELECT)
@@ -25,32 +25,35 @@
     -- Lever je werk pas in op Canvas als alle tests slagen.
 -- ------------------------------------------------------------------------
 
-
 -- S5.1.
 -- Welke medewerkers hebben zowel de Java als de XML cursus
 -- gevolgd? Geef hun personeelsnummers.
--- DROP VIEW IF EXISTS s5_1; CREATE OR REPLACE VIEW s5_1 AS                                                     -- [TEST]
+DROP VIEW IF EXISTS s5_1; CREATE OR REPLACE VIEW s5_1 AS                                                     -- [TEST]
 SELECT mnr
 FROM medewerkers
-WHERE mnr IN (SELECT cursist
-        FROM inschrijvingen
-        WHERE cursus = 'JAV'
-        OR cursus = 'XML');
+WHERE mnr IN (
+	SELECT cursist
+	FROM inschrijvingen
+	WHERE cursus = 'JAV')
+AND mnr IN(
+    SELECT cursist
+	FROM inschrijvingen
+	WHERE cursus = 'XML');
 
 -- S5.2.
 -- Geef de nummers van alle medewerkers die niet aan de afdeling 'OPLEIDINGEN'
 -- zijn verbonden.
--- DROP VIEW IF EXISTS s5_2; CREATE OR REPLACE VIEW s5_2 AS                                                     -- [TEST]
+DROP VIEW IF EXISTS s5_2; CREATE OR REPLACE VIEW s5_2 AS                                                     -- [TEST]
 SELECT mnr
-FROM medewerkers
-WHERE mnr NOT IN (SELECT hoofd
-            FROM afdelingen
-            WHERE naam = 'OPLEIDINGEN');
+FROM medewerkers med
+WHERE afd NOT IN (SELECT anr
+	FROM afdelingen afd
+	WHERE naam = 'OPLEIDINGEN');
 
 -- S5.3.
 -- Geef de nummers van alle medewerkers die de Java-cursus niet hebben
 -- gevolgd.
--- DROP VIEW IF EXISTS s5_3; CREATE OR REPLACE VIEW s5_3 AS                                                     -- [TEST]
+DROP VIEW IF EXISTS s5_3; CREATE OR REPLACE VIEW s5_3 AS                                                     -- [TEST]
 SELECT mnr
 FROM medewerkers
 WHERE mnr NOT IN (SELECT cursist
@@ -60,45 +63,81 @@ WHERE mnr NOT IN (SELECT cursist
 -- S5.4.
 -- Welke medewerkers (naam) hebben ondergeschikten? En welke niet? (Je mag twee
 -- queries gebruiken.)
--- DROP VIEW IF EXISTS s5_4; CREATE OR REPLACE VIEW s5_4 AS                                                     -- [TEST]
+DROP VIEW IF EXISTS s5_4; CREATE OR REPLACE VIEW s5_4 AS                                                     -- [TEST]
 SELECT naam
-FROM medewerkers
-WHERE mnr EXISTS (SELECT DISTINCT chef
-        FROM medewerkers);
+FROM medewerkers med
+WHERE EXISTS (
+	SELECT DISTINCT chef
+	FROM medewerkers chef
+	WHERE chef.chef = med.mnr);
+
 SELECT naam
-FROM medewerkers
-WHERE mnr NOT EXISTS (SELECT DISTINCT chef
-        FROM medewerkers);
+FROM medewerkers med
+WHERE NOT EXISTS (
+	SELECT DISTINCT chef
+	FROM medewerkers chef
+ 	WHERE chef.chef = med.mnr);
 
 -- S5.5.
 -- Geef cursuscode en begindatum van alle uitvoeringen van programmeercursussen
 -- ('BLD') in 2020.
--- DROP VIEW IF EXISTS s5_5; CREATE OR REPLACE VIEW s5_5 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s5_5; CREATE OR REPLACE VIEW s5_5 AS                                                     -- [TEST]
+SELECT cursus, begindatum
+FROM uitvoeringen
+WHERE EXTRACT(YEAR FROM begindatum) = 2020
+AND cursus IN (
+    SELECT code
+    FROM cursussen
+	WHERE type = 'BLD'
+	);
 
 -- S5.6.
 -- Geef van alle cursusuitvoeringen: de cursuscode, de begindatum en het
 -- aantal inschrijvingen (`aantal_inschrijvingen`). Sorteer op begindatum.
--- DROP VIEW IF EXISTS s5_6; CREATE OR REPLACE VIEW s5_6 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s5_6; CREATE OR REPLACE VIEW s5_6 AS                                                     -- [TEST]
+SELECT cursus, begindatum, (
+    SELECT COUNT(*)
+    FROM inschrijvingen ins
+    WHERE ins.begindatum = uitv.begindatum
+	AND ins.cursus = uitv.cursus)
+	AS aantal_inschrijvingen
+FROM uitvoeringen uitv
+ORDER BY begindatum DESC;
 
 -- S5.7.
 -- Geef voorletter(s) en achternaam van alle trainers die ooit tijdens een
 -- algemene cursus hun eigen chef als cursist hebben gehad.
--- DROP VIEW IF EXISTS s5_7; CREATE OR REPLACE VIEW s5_7 AS                                                     -- [TEST]
-
+DROP VIEW IF EXISTS s5_7; CREATE OR REPLACE VIEW s5_7 AS                                                     -- [TEST]
+SELECT trainer.voorl, trainer.naam
+FROM medewerkers trainer, inschrijvingen ins
+WHERE trainer.chef IN (
+    SELECT cursist
+    FROM  uitvoeringen uitv
+    WHERE uitv.cursus = ins.cursus
+	AND trainer.mnr = uitv.docent
+  	AND cursus IN (
+  		SELECT code
+  		FROM cursussen cursus
+  		WHERE type = 'ALG'
+  	)
+);
 
 -- S5.8.
 -- Geef de naam van de medewerkers die nog nooit een cursus hebben gegeven.
--- DROP VIEW IF EXISTS s5_8; CREATE OR REPLACE VIEW s5_8 AS                                                     -- [TEST]
-
-
+DROP VIEW IF EXISTS s5_8; CREATE OR REPLACE VIEW s5_8 AS                                                     -- [TEST]
+SELECT naam
+FROM medewerkers med
+WHERE NOT EXISTS(
+    SELECT docent
+    FROM uitvoeringen uitv
+    WHERE med.mnr = uitv.docent
+);
 
 -- -------------------------[ HU TESTRAAMWERK ]--------------------------------
     -- Met onderstaande query kun je je code testen. Zie bovenaan dit bestand
 -- voor uitleg.
 
-    SELECT * FROM test_select('S5.1') AS resultaat
+SELECT * FROM test_select('S5.1') AS resultaat
 UNION
 SELECT * FROM test_select('S5.2') AS resultaat
 UNION
