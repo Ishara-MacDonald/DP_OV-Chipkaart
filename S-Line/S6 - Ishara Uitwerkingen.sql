94c7337cd6690700edb6482051e8a7b2e3d22e34
Ishara-Mac <isharamacdonald@hotmail.com>
2020-10-07 11:55:28 +0200

    Finished S6, Worked on S7

---
+++ S-Line/S6 - Ishara Uitwerkingen.sql
@@ -0,0 +1,55 @@
+-- ------------------------------------------------------------------------
+-- Data & Persistency
+-- Opdracht S6: Views
+--
+-- (c) 2020 Hogeschool Utrecht
+-- Tijmen Muller (tijmen.muller@hu.nl)
+-- André Donk (andre.donk@hu.nl)
+-- ------------------------------------------------------------------------
+
+
+-- S6.1.
+--
+-- 1. Maak een view met de naam "deelnemers" waarmee je de volgende gegevens uit de tabellen inschrijvingen en uitvoering combineert:
+--    inschrijvingen.cursist, inschrijvingen.cursus, inschrijvingen.begindatum, uitvoeringen.docent, uitvoeringen.locatie
+
+CREATE OR REPLACE VIEW 	deelnemers AS
+SELECT ins.cursist, ins.cursus, ins.begindatum, uitv.docent, uitv.locatie
+FROM inschrijvingen ins, uitvoeringen uitv
+WHERE ins.cursus = uitv.cursus
+AND ins.begindatum = uitv.begindatum;
+
+-- 2. Gebruik de view in een query waarbij je de "deelnemers" view combineert met de "personeels" view (behandeld in de les):
+--     CREATE OR REPLACE VIEW personeel AS
+-- 	     SELECT mnr, voorl, naam as medewerker, afd, functie
+--       FROM medewerkers;
+SELECT *
+FROM deelnemers
+LEFT JOIN personeel
+ON mnr = cursist;
+-- 3. Is de view "deelnemers" updatable ? Waarom ?
+UPDATE deelnemers SET docent = 7566 WHERE cursist = 7499;
+'DETAIL: Views that do not select from a single table or view are not automatically updatable';
+
+
+-- S6.2.
+--
+-- 1. Maak een view met de naam "dagcursussen". Deze view dient de gegevens op te halen:
+--      code, omschrijving en type uit de tabel curssussen met als voorwaarde dat de lengte = 1. Toon aan dat de view werkt.
+CREATE OR REPLACE VIEW dagcursussen AS
+SELECT code, omschrijving, type
+FROM cursussen
+WHERE lengte = 1;
+
+-- 2. Maak een tweede view met de naam "daguitvoeringen".
+--    Deze view dient de uitvoeringsgegevens op te halen voor de "dagcurssussen" (gebruik ook de view "dagcursussen"). Toon aan dat de view werkt
+CREATE OR REPLACE VIEW daguitvoeringen AS
+SELECT cursus, begindatum, docent, locatie
+FROM uitvoeringen uitv
+JOIN dagcursussen dagc
+ON uitv.cursus = dagc.code
+-- 3. Verwijder de views en laat zien wat de verschillen zijn bij DROP view <viewnaam> CASCADE en bij DROP view <viewnaam> RESTRICT
+DROP VIEW dagcursussen CASCADE
+'CASCADE: Verwijdert ook de views waaraan de view is gelinkt.'
+DROP VIEW dagcursussen RESTRICT
+'RESTRICT: Verwijdert alleen de view als deze niet in een andere view wordt gebruikt.'
---
+++ S-Line/S7 - Ishara Uitwerkingen.sql
@@ -0,0 +1,126 @@
+-- ------------------------------------------------------------------------
+-- Data & Persistency
+-- Opdracht S7: Indexen
+--
+-- (c) 2020 Hogeschool Utrecht
+-- Tijmen Muller (tijmen.muller@hu.nl)
+-- André Donk (andre.donk@hu.nl)
+-- ------------------------------------------------------------------------
+-- LET OP, zoals in de opdracht op Canvas ook gezegd kun je informatie over
+-- het query plan vinden op: https://www.postgresql.org/docs/current/using-explain.html
+
+
+-- S7.1.
+--
+-- Je maakt alle opdrachten in de 'sales' database die je hebt aangemaakt en gevuld met
+-- de aangeleverde data (zie de opdracht op Canvas).
+--
+-- Voer het voorbeeld uit wat in de les behandeld is:
+-- 1. Voer het volgende EXPLAIN statement uit:
+   EXPLAIN SELECT * FROM order_lines WHERE stock_item_id = 9;
+--    Bekijk of je het resultaat begrijpt. Kopieer het explain plan onderaan de opdracht
+  ->  Parallel Seq Scan on order_lines  (cost=0.00..5051.27 rows=418 width=96)
+  '5051 duration of sorts'
+-- 2. Voeg een index op stock_item_id toe:
+   CREATE INDEX ord_lines_si_id_idx ON order_lines (stock_item_id);
+-- 3. Analyseer opnieuw met EXPLAIN hoe de query nu uitgevoerd wordt
+--    Kopieer het explain plan onderaan de opdracht
+  ->  Bitmap Index Scan on ord_lines_si_id_idx  (cost=0.00..19.94 rows=1002 width=0)
+-- 4. Verklaar de verschillen. Schrijf deze hieronder op.
+Index is sneller dan key.
+Door te zoeken met de key loopt de query door de hele database.
+Met een index erbij wordt er een techniek toegevoegd aan de database waarmee deze sneller iets kan terug vinden.
+
+
+-- S7.2.
+--
+-- 1. Maak de volgende twee query’s:
+-- 	  A. Toon uit de order tabel de order met order_id = 73590
+SELECT * FROM orders WHERE order_id = 73590
+-- 	  B. Toon uit de order tabel de order met customer_id = 1028
+SELECT * FROM orders WHERE customer_id = 1028
+-- 2. Analyseer met EXPLAIN hoe de query’s uitgevoerd worden en
+--    kopieer het explain plan onderaan de opdracht
+A. Index Scan using pk_sales_orders on orders  (cost=0.29..8.31 rows=1 width=155)
+B. Seq Scan on orders  (cost=0.00..1819.94 rows=107 width=155)
+-- 3. Verklaar de verschillen en schrijf deze op
+Verschil 1: Index Scan vs. Seq Scan
+order_id is een primary key en heeft een id en dus al een index. Dit betekent dat elk order_id uniek is en maar één keer kan voorkomen in dit tabel.
+De database weet dit en zoekt op basis van PK.
+customer_id is geen PK en kan meerdere keren voorkomen, dus moet de database voor deze query de hele tabel checken.
+-- 4. Voeg een index toe, waarmee query B versneld kan worden
+CREATE INDEX customer_id_idx ON orders (customer_id);
+-- 5. Analyseer met EXPLAIN en kopieer het explain plan onder de opdracht
+  ->  Bitmap Index Scan on customer_id_idx  (cost=0.00..5.10 rows=107 width=0)
+-- 6. Verklaar de verschillen en schrijf hieronder op
+Index is sneller dan key.
+Door te zoeken met de key loopt de query door de hele database.
+Met een index erbij wordt er een techniek toegevoegd aan de database waarmee deze sneller iets kan terug vinden.
+
+-- S7.3.A
+--
+-- Het blijkt dat customers regelmatig klagen over trage bezorging van hun bestelling.
+-- Het idee is dat verkopers misschien te lang wachten met het invoeren van de bestelling in het systeem.
+-- Daar willen we meer inzicht in krijgen.
+-- We willen alle orders (order_id, order_date, salesperson_person_id (als verkoper),
+--    het verschil tussen expected_delivery_date en order_date (als levertijd),
+--    en de bestelde hoeveelheid van een product zien (quantity uit order_lines).
+-- Dit willen we alleen zien voor een bestelde hoeveelheid van een product > 250
+--   (we zijn nl. als eerste geïnteresseerd in grote aantallen want daar lijkt het vaker mis te gaan)
+-- En verder willen we ons focussen op verkopers wiens bestellingen er gemiddeld langer over doen.
+-- De meeste bestellingen kunnen binnen een dag bezorgd worden, sommige binnen 2-3 dagen.
+-- Het hele bestelproces is er op gericht dat de gemiddelde bestelling binnen 1.45 dagen kan worden bezorgd.
+-- We willen in onze query dan ook alleen de verkopers zien wiens gemiddelde levertijd
+--  (expected_delivery_date - order_date) over al zijn/haar bestellingen groter is dan 1.45 dagen.
+-- Maak om dit te bereiken een subquery in je WHERE clause.
+-- Sorteer het resultaat van de hele geheel op levertijd (desc) en verkoper.
+-- 1. Maak hieronder deze query (als je het goed doet zouden er 377 rijen uit moeten komen, en het kan best even duren...)
+
+levertijd:
+SELECT expected_delivery_date - order_date
+FROM orders
+
+totaal_aantal_producten:
+SELECT order_id, (
+    SELECT SUM(quantity)
+    FROM order_lines
+    GROUP BY order_id
+    HAVING orders.order_id = order_lines.order_id
+    )
+FROM orders
+
+SELECT order_id, order_date, salesperson_person_id AS verkoper, levertijd,
+    (
+    SELECT SUM(quantity)
+    FROM order_lines
+    GROUP BY order_id
+    HAVING orders.order_id = order_lines.order_id
+    ) AS totaal_aantal_producten
+FROM orders, levertijd
+WHERE EXISTS (
+    SELECT SUM(quantity)
+    FROM order_lines
+    GROUP BY order_id
+    HAVING orders.order_id = order_lines.order_id
+    AND SUM(quantity) > 250
+    )
+AND ( expected_delivery_date - order_date) > 1.45
+
+ORDER BY levertijd DESC and verkoper
+
+CREATE INDEX ord_lines_si_id_idx ON order_lines (stock_item_id);
+
+-- S7.3.B
+--
+-- 1. Vraag het EXPLAIN plan op van je query (kopieer hier, onder de opdracht)
+-- 2. Kijk of je met 1 of meer indexen de query zou kunnen versnellen
+-- 3. Maak de index(en) aan en run nogmaals het EXPLAIN plan (kopieer weer onder de opdracht)
+-- 4. Wat voor verschillen zie je? Verklaar hieronder.
+
+
+
+-- S7.3.C
+--
+-- Zou je de query ook heel anders kunnen schrijven om hem te versnellen?
+
+
